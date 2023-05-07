19c97211a393928d3b0bf15c42908e77495df571
jose-cleiton <cleitons835@gmail.com>
2023-05-07 19:37:15 -0300

    feat: :sparkles: fixing fonts images and etc

--- src/front-end/src/posts/PostsList.js
+++ src/front-end/src/posts/PostsList.js
@@ -3,28 +3,27 @@ import StyledPosts from './styles';
 
 const PostsList = ({ posts }) => {
   return (
-    <div className='post'>
-     
-      {posts.map(post => (
-        <div key={post.id}>
-          <img src={post.images[0]} alt={post.titulo} width={160} />
-          <div className="postInfo">
-            <div className="postCats">
-              <span className="postCat"></span>
-              <span className="postCat"></span>              
+    <StyledPosts>
+      <div className='posts'>
+        {posts.map(post => (
+          <div key={post.id}>
+            {post.images.length > 0 && (
+              <img src={post.images[0]} alt={post.titulo} className='postImg'/>
+            )}
+            <div className="postInfo">
+              <div className="postCats">
+                <span className="postCat"></span>
+                <span className="postCat"></span>              
+              </div>
+              <span className="postTitle">{post.titulo}</span>
+              <hr />
+              <span className="postDate">{new Date(post.data_publicacao).toLocaleDateString()}</span>
+              <p className='postDesc'>{post.content}</p>
             </div>
-            <span className="postTitle">{post.titulo}</span>
-            <hr />
-            <span className="postDate">{new Date(post.data_publicacao).toLocaleDateString()}</span>
-            <p>{post.content}</p>
-
-          
           </div>
-         
-          
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </StyledPosts>
   );
 };
 
